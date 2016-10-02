212742446ba7888d379b5525d103c07ff59fb55e
长宏 <3200601@qq.com>
2016-10-02 20:04:36 +0800

    基本完成改版

--- src/mysqls/ui_mainitem/ObjectSerchPanel.java
+++ src/mysqls/ui_mainitem/ObjectSerchPanel.java
@@ -1,16 +1,26 @@
 package mysqls.ui_mainitem;
 
-import mysqls.sql.databaseserver2.MYtreeNode;
-import mysqls.sql.databaseserver2.MYtreeNodeRoot;
+import mysqls.contanst.ConnectINFO;
+import mysqls.contanst.ConnectINFOListener;
+import mysqls.sql.databaseserver2.*;
+import mysqls.ui_frame.EmptyPanel;
 
 import javax.swing.*;
 import javax.swing.MYtreelist_cell_render;
+import javax.swing.event.DocumentEvent;
+import javax.swing.event.DocumentListener;
+import javax.swing.text.BadLocationException;
+import javax.swing.tree.DefaultMutableTreeNode;
+import javax.swing.tree.TreePath;
 import java.awt.*;
+import java.util.*;
+import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * Created by 长宏 on 2016/10/1 0001.
  */
-public class ObjectSerchPanel extends JPanel {
+public class ObjectSerchPanel extends JPanel implements ConnectINFOListener{
     private static ObjectSerchPanel me=null;
     public static ObjectSerchPanel getInstance() {
         if (me == null) {
@@ -19,50 +29,113 @@ public class ObjectSerchPanel extends JPanel {
         return me;
     }
 
+    java.util.List<MYtreeNode> mYtreeNodeList = new ArrayList<>();
     private JList<MYtreeNode> jList;
     DefaultListModel<MYtreeNode> listModel;
     ListCellRenderer cellRenderer;
     private ObjectSerchPanel() {
+        ConnectINFO.addLister(this);
+//        setdata();
+        if (ConnectINFO.getInstance().getConnection() == null) {
+            JLabel text ;
+            Icon icon = new ImageIcon(EmptyPanel.class.getClassLoader().getResource("database/datas.png"));
+            JLabel image = new JLabel(icon);
+            text = new JLabel("请先链接数据库！");
+            text.setHorizontalAlignment(SwingConstants.CENTER);
+            Font font = new Font(Font.MONOSPACED, Font.BOLD, 30);
+            text.setFont(font);
+            text.setVerticalAlignment(SwingConstants.CENTER);
+            add(image, BorderLayout.CENTER);
+            add(text, BorderLayout.NORTH);
+            return;
+        }
+
+
+    }
+
+    private static String seachtext=null;
+    private List<MYtreeNode> dosearch(String string) {
+        seachtext=string;
+        System.out.println(string);
+        List<MYtreeNode> list = mYtreeNodeList.stream().filter(ObjectSerchPanel::searchfilster).collect(Collectors.toList());
+        return list;
+    }
+
+    private static boolean searchfilster(MYtreeNode mYtreeNode) {
+        if (mYtreeNode.getName().contains(seachtext)) {
+            return true;
+        }
+        return false;
+    }
+
+
+    private void setdata() {
+        if (mYtreeNodeList.size() > 0) {
+            return;
+        }
+        mYtreeNodeList.clear();
+        MYtreeNodeRoot mYtreeNodeRoot = new MYtreeNodeRoot("root");
+        for (MYtreeNodeDB db : mYtreeNodeRoot.getdbs()) {
+            mYtreeNodeList.add(db);
+            for (MYtreeNodeTable table : db.geTables()) {
+                mYtreeNodeList.add(table);
+                for (MYtreeNodeColumn column : table.getcolumns()) {
+                    mYtreeNodeList.add(column);
+                }
+
+            }
+        }
+
+    }
+
+    @Override
+    public void onchange(String name, Object news, Object oldies) {
+        if (name.equals(ConnectINFO.CONNECTION)) {
+            System.out.println(name);
+            setuimy();
+        }
+
+    }
+
+    private void setuimy() {
+        removeAll();
         listModel = new DefaultListModel<>();
         cellRenderer = new MYtreelist_cell_render();
         jList = new JList(listModel);
         jList.setCellRenderer(cellRenderer);
-        listModel.addElement(new MYtreeNodeRoot("eeee"));
-        listModel.addElement(new MYtreeNodeRoot("eedee"));
-        listModel.addElement(new MYtreeNodeRoot("eedee"));
-        listModel.addElement(new MYtreeNodeRoot("eedee"));
-        listModel.addElement(new MYtreeNodeRoot("eeeddde"));  listModel.addElement(new MYtreeNodeRoot("eeee"));
-        listModel.addElement(new MYtreeNodeRoot("eedee"));
-        listModel.addElement(new MYtreeNodeRoot("eedee"));
-        listModel.addElement(new MYtreeNodeRoot("eedee"));
-        listModel.addElement(new MYtreeNodeRoot("eeeddde"));  listModel.addElement(new MYtreeNodeRoot("eeee"));
-        listModel.addElement(new MYtreeNodeRoot("eedee"));
-        listModel.addElement(new MYtreeNodeRoot("eedee"));
-        listModel.addElement(new MYtreeNodeRoot("eedee"));
-        listModel.addElement(new MYtreeNodeRoot("eeeddde"));  listModel.addElement(new MYtreeNodeRoot("eeee"));
-        listModel.addElement(new MYtreeNodeRoot("eedee"));
-        listModel.addElement(new MYtreeNodeRoot("eedee"));
-        listModel.addElement(new MYtreeNodeRoot("eedee"));
-        listModel.addElement(new MYtreeNodeRoot("eeeddde"));  listModel.addElement(new MYtreeNodeRoot("eeee"));
-        listModel.addElement(new MYtreeNodeRoot("eedee"));
-        listModel.addElement(new MYtreeNodeRoot("eedee"));
-        listModel.addElement(new MYtreeNodeRoot("eedee"));
-        listModel.addElement(new MYtreeNodeRoot("eeeddde"));  listModel.addElement(new MYtreeNodeRoot("eeee"));
-        listModel.addElement(new MYtreeNodeRoot("eedee"));
-        listModel.addElement(new MYtreeNodeRoot("eedee"));
-        listModel.addElement(new MYtreeNodeRoot("eedee"));
-        listModel.addElement(new MYtreeNodeRoot("eeeddde"));  listModel.addElement(new MYtreeNodeRoot("eeee"));
-        listModel.addElement(new MYtreeNodeRoot("eedee"));
-        listModel.addElement(new MYtreeNodeRoot("eedee"));
-        listModel.addElement(new MYtreeNodeRoot("eedee"));
-        listModel.addElement(new MYtreeNodeRoot("eeeddde"));  listModel.addElement(new MYtreeNodeRoot("eeee"));
-        listModel.addElement(new MYtreeNodeRoot("eedee"));
-        listModel.addElement(new MYtreeNodeRoot("eedee"));
-        listModel.addElement(new MYtreeNodeRoot("eedee"));
-        listModel.addElement(new MYtreeNodeRoot("eeeddde"));
 
+        JTextField field = new JTextField(20);
+        field.getDocument().addDocumentListener(new DocumentListener() {
+            @Override
+            public void insertUpdate(DocumentEvent e) {
+                listModel.clear();
+                setdata();
+                List<MYtreeNode> list= null;
+                list = dosearch(field.getText());
+                list.forEach(a->listModel.addElement(a));
+
+            }
+
+            @Override
+            public void removeUpdate(DocumentEvent e) {
+                listModel.clear();
+                setdata();
+                List<MYtreeNode> list= null;
+                list = dosearch(field.getText());
+                list.forEach(a->listModel.addElement(a));
+            }
+
+            @Override
+            public void changedUpdate(DocumentEvent e) {
+
+            }
+        });
         setLayout(new BorderLayout());
         add(new JScrollPane(jList));
+        JPanel nouth = new JPanel(new GridLayout(1, 0));
+        nouth.add(field);
+        nouth.add(new JLabel("输入文字搜索"));
+        add(nouth, BorderLayout.NORTH);
 
     }
 }
--- src/mysqls/ui_util/MYtreelist_cell_render.java
+++ src/mysqls/ui_util/MYtreelist_cell_render.java
@@ -25,87 +25,29 @@
 
 package javax.swing;
 
-import sun.swing.DefaultLookup;
+import mysqls.sql.databaseserver2.MYtreeNode;
+import mysqls.sql.databaseserver2.MYtreeNodeColumn;
+import mysqls.sql.databaseserver2.MYtreeNodeDB;
+import mysqls.sql.databaseserver2.MYtreeNodeTable;
 
-import javax.swing.border.Border;
-import javax.swing.border.EmptyBorder;
+import javax.swing.tree.DefaultMutableTreeNode;
+import javax.swing.tree.DefaultTreeModel;
+import javax.swing.tree.TreePath;
 import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 import java.io.Serializable;
 
 
-/**
- * Renders an item in a list.
- * <p>
- * <strong><a name="override">Implementation Note:</a></strong>
- * This class overrides
- * <code>invalidate</code>,
- * <code>validate</code>,
- * <code>revalidate</code>,
- * <code>repaint</code>,
- * <code>isOpaque</code>,
- * and
- * <code>firePropertyChange</code>
- * solely to improve performance.
- * If not overridden, these frequently called methods would execute code paths
- * that are unnecessary for the default list cell renderer.
- * If you write your own renderer,
- * take care to weigh the benefits and
- * drawbacks of overriding these methods.
- *
- * <p>
- *
- * <strong>Warning:</strong>
- * Serialized objects of this class will not be compatible with
- * future Swing releases. The current serialization support is
- * appropriate for short term storage or RMI between applications running
- * the same version of Swing.  As of 1.4, support for long term storage
- * of all JavaBeans&trade;
- * has been added to the <code>java.beans</code> package.
- * Please see {@link java.beans.XMLEncoder}.
- *
- * @author Philip Milne
- * @author Hans Muller
- */
-public class MYtreelist_cell_render extends JLabel
+public class MYtreelist_cell_render
         implements ListCellRenderer<Object>, Serializable
 {
+  static   DefaultTreeModel treeModel = new DefaultTreeModel(null);
+    static JTree jTree=null;
+    static {
+        jTree = new JTree(treeModel);
 
-    /**
-     * An empty <code>Border</code>. This field might not be used. To change the
-     * <code>Border</code> used by this renderer override the
-     * <code>getListCellRendererComponent</code> method and set the border
-     * of the returned component directly.
-     */
-    private static final Border SAFE_NO_FOCUS_BORDER = new EmptyBorder(1, 1, 1, 1);
-    private static final Border DEFAULT_NO_FOCUS_BORDER = new EmptyBorder(1, 1, 1, 1);
-    protected static Border noFocusBorder = DEFAULT_NO_FOCUS_BORDER;
-
-    /**
-     * Constructs a default renderer object for an item
-     * in a list.
-     */
-    public MYtreelist_cell_render() {
-        super();
-        setOpaque(true);
-        setBorder(getNoFocusBorder());
-        setName("List.cellRenderer");
     }
-
-    private Border getNoFocusBorder() {
-        Border border = DefaultLookup.getBorder(this, ui, "List.cellNoFocusBorder");
-        if (System.getSecurityManager() != null) {
-            if (border != null) return border;
-            return SAFE_NO_FOCUS_BORDER;
-        } else {
-            if (border != null &&
-                    (noFocusBorder == null ||
-                            noFocusBorder == DEFAULT_NO_FOCUS_BORDER)) {
-                return border;
-            }
-            return noFocusBorder;
-        }
-    }
-
     public Component getListCellRendererComponent(
             JList<?> list,
             Object value,
@@ -113,232 +55,38 @@ public class MYtreelist_cell_render extends JLabel
             boolean isSelected,
             boolean cellHasFocus)
     {
-        setComponentOrientation(list.getComponentOrientation());
-
-        Color bg = null;
-        Color fg = null;
-
-        JList.DropLocation dropLocation = list.getDropLocation();
-        if (dropLocation != null
-                && !dropLocation.isInsert()
-                && dropLocation.getIndex() == index) {
-
-            bg = DefaultLookup.getColor(this, ui, "List.dropCellBackground");
-            fg = DefaultLookup.getColor(this, ui, "List.dropCellForeground");
-
-            isSelected = true;
+        if (value instanceof MYtreeNodeDB) {
+            DefaultMutableTreeNode root=new DefaultMutableTreeNode(value.toString());
+            treeModel.setRoot(root);
+            return jTree;
         }
+        if (value instanceof MYtreeNodeTable) {
+            MYtreeNodeTable mYtreeNodeTable= (MYtreeNodeTable) value;
+            DefaultMutableTreeNode root=new DefaultMutableTreeNode(mYtreeNodeTable.getDb().toString());
+            DefaultMutableTreeNode defatable = new DefaultMutableTreeNode(mYtreeNodeTable.toString());
+            root.add(defatable);
+            treeModel.setRoot(root);
+            TreePath treePath = new TreePath(new Object[]{root});
+            jTree.expandPath(treePath);
+            return jTree;
 
-        if (isSelected) {
-            setBackground(bg == null ? list.getSelectionBackground() : bg);
-            setForeground(fg == null ? list.getSelectionForeground() : fg);
         }
-        else {
-            setBackground(list.getBackground());
-            setForeground(list.getForeground());
-        }
+        if (value instanceof MYtreeNodeColumn) {
+            DefaultMutableTreeNode child = new DefaultMutableTreeNode(value.toString());
+            MYtreeNodeColumn mYtreeNodeTable= (MYtreeNodeColumn) value;
+            DefaultMutableTreeNode root=new DefaultMutableTreeNode(mYtreeNodeTable.getTable().getDb().toString());
+        DefaultMutableTreeNode defatable = new DefaultMutableTreeNode(mYtreeNodeTable.getTable().toString());
+            root.add(defatable);
+            defatable.add(child);
+            treeModel.setRoot(root);
 
-        if (value instanceof Icon) {
-            setIcon((Icon)value);
-            setText("");
-        }
-        else {
-            setIcon(null);
-            setText((value == null) ? "" : value.toString());
-        }
+            TreePath treePath = new TreePath(new Object[]{root, defatable});
+            jTree.expandPath(treePath);
+            return jTree;
 
-        setEnabled(list.isEnabled());
-        setFont(list.getFont());
-
-        Border border = null;
-        if (cellHasFocus) {
-            if (isSelected) {
-                border = DefaultLookup.getBorder(this, ui, "List.focusSelectedCellHighlightBorder");
-            }
-            if (border == null) {
-                border = DefaultLookup.getBorder(this, ui, "List.focusCellHighlightBorder");
-            }
-        } else {
-            border = getNoFocusBorder();
         }
-        setBorder(border);
-
-        return this;
+        return jTree;
     }
 
-    /**
-     * Overridden for performance reasons.
-     * See the <a href="#override">Implementation Note</a>
-     * for more information.
-     *
-     * @since 1.5
-     * @return <code>true</code> if the background is completely opaque
-     *         and differs from the JList's background;
-     *         <code>false</code> otherwise
-     */
-    @Override
-    public boolean isOpaque() {
-        Color back = getBackground();
-        Component p = getParent();
-        if (p != null) {
-            p = p.getParent();
-        }
-        // p should now be the JList.
-        boolean colorMatch = (back != null) && (p != null) &&
-                back.equals(p.getBackground()) &&
-                p.isOpaque();
-        return !colorMatch && super.isOpaque();
-    }
 
-    /**
-     * Overridden for performance reasons.
-     * See the <a href="#override">Implementation Note</a>
-     * for more information.
-     */
-    @Override
-    public void validate() {}
-
-    /**
-     * Overridden for performance reasons.
-     * See the <a href="#override">Implementation Note</a>
-     * for more information.
-     *
-     * @since 1.5
-     */
-    @Override
-    public void invalidate() {}
-
-    /**
-     * Overridden for performance reasons.
-     * See the <a href="#override">Implementation Note</a>
-     * for more information.
-     *
-     * @since 1.5
-     */
-    @Override
-    public void repaint() {}
-
-    /**
-     * Overridden for performance reasons.
-     * See the <a href="#override">Implementation Note</a>
-     * for more information.
-     */
-    @Override
-    public void revalidate() {}
-    /**
-     * Overridden for performance reasons.
-     * See the <a href="#override">Implementation Note</a>
-     * for more information.
-     */
-    @Override
-    public void repaint(long tm, int x, int y, int width, int height) {}
-
-    /**
-     * Overridden for performance reasons.
-     * See the <a href="#override">Implementation Note</a>
-     * for more information.
-     */
-    @Override
-    public void repaint(Rectangle r) {}
-
-    /**
-     * Overridden for performance reasons.
-     * See the <a href="#override">Implementation Note</a>
-     * for more information.
-     */
-    @Override
-    protected void firePropertyChange(String propertyName, Object oldValue, Object newValue) {
-        // Strings get interned...
-        if (propertyName == "text"
-                || ((propertyName == "font" || propertyName == "foreground")
-                && oldValue != newValue
-                && getClientProperty(javax.swing.plaf.basic.BasicHTML.propertyKey) != null)) {
-
-            super.firePropertyChange(propertyName, oldValue, newValue);
-        }
-    }
-
-    /**
-     * Overridden for performance reasons.
-     * See the <a href="#override">Implementation Note</a>
-     * for more information.
-     */
-    @Override
-    public void firePropertyChange(String propertyName, byte oldValue, byte newValue) {}
-
-    /**
-     * Overridden for performance reasons.
-     * See the <a href="#override">Implementation Note</a>
-     * for more information.
-     */
-    @Override
-    public void firePropertyChange(String propertyName, char oldValue, char newValue) {}
-
-    /**
-     * Overridden for performance reasons.
-     * See the <a href="#override">Implementation Note</a>
-     * for more information.
-     */
-    @Override
-    public void firePropertyChange(String propertyName, short oldValue, short newValue) {}
-
-    /**
-     * Overridden for performance reasons.
-     * See the <a href="#override">Implementation Note</a>
-     * for more information.
-     */
-    @Override
-    public void firePropertyChange(String propertyName, int oldValue, int newValue) {}
-
-    /**
-     * Overridden for performance reasons.
-     * See the <a href="#override">Implementation Note</a>
-     * for more information.
-     */
-    @Override
-    public void firePropertyChange(String propertyName, long oldValue, long newValue) {}
-
-    /**
-     * Overridden for performance reasons.
-     * See the <a href="#override">Implementation Note</a>
-     * for more information.
-     */
-    @Override
-    public void firePropertyChange(String propertyName, float oldValue, float newValue) {}
-
-    /**
-     * Overridden for performance reasons.
-     * See the <a href="#override">Implementation Note</a>
-     * for more information.
-     */
-    @Override
-    public void firePropertyChange(String propertyName, double oldValue, double newValue) {}
-
-    /**
-     * Overridden for performance reasons.
-     * See the <a href="#override">Implementation Note</a>
-     * for more information.
-     */
-    @Override
-    public void firePropertyChange(String propertyName, boolean oldValue, boolean newValue) {}
-
-    /**
-     * A subclass of MYtreelist_cell_render that implements UIResource.
-     * MYtreelist_cell_render doesn't implement UIResource
-     * directly so that applications can safely override the
-     * cellRenderer property with MYtreelist_cell_render subclasses.
-     * <p>
-     * <strong>Warning:</strong>
-     * Serialized objects of this class will not be compatible with
-     * future Swing releases. The current serialization support is
-     * appropriate for short term storage or RMI between applications running
-     * the same version of Swing.  As of 1.4, support for long term storage
-     * of all JavaBeans&trade;
-     * has been added to the <code>java.beans</code> package.
-     * Please see {@link java.beans.XMLEncoder}.
-     */
-    public static class UIResource extends MYtreelist_cell_render
-            implements javax.swing.plaf.UIResource
-    {
-    }
 }
